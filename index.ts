2f0eaab85cdc81ce470d92bc482bc0697c808da3
Ho-Ping Keung <ho-ping.keung@student.ap.be>
2024-04-24 08:46:09 +0000

    catch, guess, landing en forgotpw gerendert in ejs

--- index.ts
+++ index.ts
@@ -7,6 +7,13 @@ app.set("view engine", "ejs");
 app.set("port", 3000);
 app.use(express.static("public"));
 
+app.get("/landing", (req, res) => {
+    res.render("landing");
+})
+
+app.get("/forgot", (req, res) => {
+    res.render("forgot");
+})
 
 app.get("/", (req, res) => {
     res.render("index");
@@ -19,6 +26,14 @@ app.get("/pokedex", (req, res) => {
 app.get("/battle", (req, res) => {
     res.render("battle");
 });
+
+app.get("/guesser", (req, res) => {
+    res.render("guesser");
+})
+
+app.get("/safari", (req, res) => {
+    res.render("safari");
+})
 
 app.listen(app.get("port"), () => {
     console.log(`Server is running on port ${app.get("port")}`);
