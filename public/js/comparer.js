92edc2b9f138252d0cb8ec5cf14eea81456f5d56
Zakaria El Jattari <zakaria.eljattari@student.ap.be>
2024-05-29 08:35:48 +0000

    .

--- public/js/comparer.js
+++ public/js/comparer.js
@@ -6,11 +6,11 @@ let poke2 = document.getElementsByClassName("clas");
 
 for(let i = 0; i < 6; i++){
 
-    if(parseInt(poke1[i].textContent)> parseInt(poke2[i].textContent)){
+    if(parseInt(poke1[i].innerHTML)> parseInt(poke2[i].innerHTML)){
         poke1.Style.backgroundColor = "green";
         poke2.style.backgroundColor = "red"
     }
-    else if(parseInt(poke1[i].textContent)< parseInt(poke2[i].textContent)){
+    else if(parseInt(poke1[i].innerHTML)< parseInt(poke2[i].innerHTML)){
         poke1.style.backgroundColor = "red";
         poke2.style.backgroundColor = "green"
     }
